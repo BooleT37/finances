c6f6aa4879cada000c8b094fda7e58b7b6900699
Alexey Levin <boolet37@gmail.com>
2022-05-02 18:39:59 +0200

    Initial setup

---
+++ src/configureMobx.ts
@@ -0,0 +1,11 @@
+import { configure } from "mobx"
+
+export default function configureMobx() {
+    configure({
+        enforceActions: "always",
+        computedRequiresReaction: true,
+        reactionRequiresObservable: true,
+        observableRequiresReaction: true,
+        disableErrorBoundaries: true
+    })
+}
