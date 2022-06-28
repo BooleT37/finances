3816776c363d3469874a797fa72fbf4fe386ca36
Alexey Levin <alexey.levin@klarna.com>
2022-06-28 22:41:09 +0200

    Transfer pers expense button

---
+++ src/PlanningScreen/TransferPeButtonRenderer.tsx
@@ -0,0 +1,30 @@
+import {ICellRendererParams} from 'ag-grid-enterprise';
+import {Button} from 'antd';
+import React from 'react'
+import {RightSquareOutlined} from '@ant-design/icons';
+import forecastStore from "../stores/forecastStore";
+import categories from "../categories";
+import {PersonalExpCategoryIds} from "../utils/constants";
+
+// eslint-disable-next-line mobx/missing-observer
+const TransferPeButtonRenderer: React.FC<ICellRendererParams> = (props) => {
+  if (props.data.category === 'Всего') {
+    return null
+  }
+  const { year, month } = props.context
+  const categoryId = categories.getByName(props.data.category).id
+  if ([PersonalExpCategoryIds.Alexey, PersonalExpCategoryIds.Lena].includes(categoryId)) {
+    return (
+      <Button
+        title="Перенести с пред. месяца"
+        icon={<RightSquareOutlined/>}
+        onClick={() => {
+          forecastStore.transferPersonalExpense(categoryId, month, year)
+        }}
+      />
+    )
+  }
+  return null
+}
+
+export default TransferPeButtonRenderer;
