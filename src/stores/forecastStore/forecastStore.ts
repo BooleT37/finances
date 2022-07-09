d9cb069bbffd332fabe0a60c378980c9341738d0
Alexey Levin <boolet37@gmail.com>
2022-07-09 13:47:08 +0200

    Fix transferred personal expenses not being saved

--- src/stores/forecastStore/forecastStore.ts
+++ src/stores/forecastStore/forecastStore.ts
@@ -1,34 +1,34 @@
-import { action, flow, makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 import Forecast from "../../models/Forecast";
 import categories from "../../readonlyStores/categories";
 import expenseStore from "../expenseStore";
-import { computedFn } from 'mobx-utils'
+import { computedFn } from "mobx-utils";
 import Category from "../../models/Category";
 import { getPreviousMonth, avgForNonEmpty } from "./utils";
 import { countUniqueMonths, roundCost, sum } from "../../utils";
-import {PersonalExpCategoryIds} from "../../utils/constants";
+import { PersonalExpCategoryIds } from "../../utils/constants";
 
 interface ForecastTableItem {
-  category: string,
-  isIncome: boolean,
-  average: number,
-  monthsWithSpendings: string,
-  lastMonth: number,
-  thisMonth: number,
-  sum: number,
-  comment: string,
+  category: string;
+  isIncome: boolean;
+  average: number;
+  monthsWithSpendings: string;
+  lastMonth: number;
+  thisMonth: number;
+  sum: number;
+  comment: string;
 }
 
 interface ForecastJson {
-  category_id: number,
-  month: number,
-  year: number,
-  sum: number,
-  comment?: string
+  category_id: number;
+  month: number;
+  year: number;
+  sum: number;
+  comment?: string;
 }
 
 class ForecastStore {
-  public forecasts = observable.array<Forecast>()
+  public forecasts = observable.array<Forecast>();
 
   constructor() {
     makeObservable(this, {
@@ -37,165 +37,215 @@ class ForecastStore {
       tableData: false,
       categoriesForecast: false,
       fromJson: action,
+      changeForecastSum: action,
+      changeForecastComment: action,
       transferPersonalExpense: action,
-      changeForecastSum: flow.bound,
-      changeForecastComment: flow.bound
-    })
+    });
   }
-  
+
   find(year: number, month: number, category: Category) {
-    return this.forecasts.find(f => f.year === year && f.month === month && f.category.id === category.id)
+    return this.forecasts.find(
+      (f) =>
+        f.year === year && f.month === month && f.category.id === category.id
+    );
   }
 
   tableData = computedFn((year: number, month: number): ForecastTableItem[] => {
-    const filtered = this.forecasts
-      .filter(forecast => forecast.month === month && forecast.year === year)
-    categories.getAll().forEach(category => {
-      if (filtered.every(f => f.category.id !== category.id)) {
-        filtered.push(new Forecast(category, month, year, 0))
+    const filtered = this.forecasts.filter(
+      (forecast) => forecast.month === month && forecast.year === year
+    );
+    categories.getAll().forEach((category) => {
+      if (filtered.every((f) => f.category.id !== category.id)) {
+        filtered.push(new Forecast(category, month, year, 0));
       }
-    })
-    filtered.sort((f1, f2) => f1.category.id - f2.category.id)
-    const data = filtered.map(forecast => ({
+    });
+    filtered.sort((f1, f2) => f1.category.id - f2.category.id);
+    const data = filtered.map((forecast) => ({
       category: forecast.category.name,
       isIncome: forecast.category.isIncome,
-      average: avgForNonEmpty(expenseStore.expenses
-        .filter(e => e.category.id === forecast.category.id)
-        .map(e => e.cost || 0)),
-      monthsWithSpendings: `${countUniqueMonths(expenseStore.expenses
-        .filter(e => e.category.id === forecast.category.id)
-        .map(e => e.date))} / ${expenseStore.totalMonths} месяцев`,
-      lastMonth: this.forecasts.find(
-        ({ category, month, year }) => category === forecast.category
-          && month === getPreviousMonth(forecast.month, forecast.year).month
-          && year === getPreviousMonth(forecast.month, forecast.year).year
-      )?.sum ?? 0,
-      thisMonth: roundCost(expenseStore.expenses
-        .filter(e => e.date.month() === month && e.date.year() === year && e.category.id === forecast.category.id)
-        .reduce((a, c) => a + (c.cost || 0), 0)),
+      average: avgForNonEmpty(
+        expenseStore.expenses
+          .filter((e) => e.category.id === forecast.category.id)
+          .map((e) => e.cost || 0)
+      ),
+      monthsWithSpendings: `${countUniqueMonths(
+        expenseStore.expenses
+          .filter((e) => e.category.id === forecast.category.id)
+          .map((e) => e.date)
+      )} / ${expenseStore.totalMonths} месяцев`,
+      lastMonth:
+        this.forecasts.find(
+          ({ category, month, year }) =>
+            category === forecast.category &&
+            month === getPreviousMonth(forecast.month, forecast.year).month &&
+            year === getPreviousMonth(forecast.month, forecast.year).year
+        )?.sum ?? 0,
+      thisMonth: roundCost(
+        expenseStore.expenses
+          .filter(
+            (e) =>
+              e.date.month() === month &&
+              e.date.year() === year &&
+              e.category.id === forecast.category.id
+          )
+          .reduce((a, c) => a + (c.cost || 0), 0)
+      ),
       sum: forecast.sum,
-      comment: forecast.comment || ''
-    }))
+      comment: forecast.comment || "",
+    }));
 
     data.push({
-      average: roundCost(sum(data.map(d => d.isIncome ? -d.average : d.average))),
-      monthsWithSpendings: '',
-      category: 'Всего',
+      average: roundCost(
+        sum(data.map((d) => (d.isIncome ? -d.average : d.average)))
+      ),
+      monthsWithSpendings: "",
+      category: "Всего",
       isIncome: false,
-      comment: '',
-      lastMonth: sum(data.map(d => d.isIncome ? -d.lastMonth : d.lastMonth)),
-      sum: sum(data.map(d => d.isIncome ? -d.sum : d.sum)),
-      thisMonth: sum(data.map(d => d.isIncome ? -d.thisMonth : d.thisMonth))
-    })
+      comment: "",
+      lastMonth: sum(
+        data.map((d) => (d.isIncome ? -d.lastMonth : d.lastMonth))
+      ),
+      sum: sum(data.map((d) => (d.isIncome ? -d.sum : d.sum))),
+      thisMonth: sum(
+        data.map((d) => (d.isIncome ? -d.thisMonth : d.thisMonth))
+      ),
+    });
 
-    return data
-  })
+    return data;
+  });
 
-  categoriesForecast = computedFn((year: number, month: number): Record<number, number> => {
-    return Object.fromEntries(
-      this.forecasts
-        .filter(f => f.month === month && f.year === year)
-        .map(f => [f.category.id, f.sum])
-    )
-  })
+  categoriesForecast = computedFn(
+    (year: number, month: number): Record<number, number> => {
+      return Object.fromEntries(
+        this.forecasts
+          .filter((f) => f.month === month && f.year === year)
+          .map((f) => [f.category.id, f.sum])
+      );
+    }
+  );
 
   fromJson(json: ForecastJson[]) {
-    this.forecasts.replace(json.map(f => new Forecast(
-      categories.getById(f.category_id),
-      f.month,
-      f.year,
-      f.sum,
-      f.comment
-    )))
+    this.forecasts.replace(
+      json.map(
+        (f) =>
+          new Forecast(
+            categories.getById(f.category_id),
+            f.month,
+            f.year,
+            f.sum,
+            f.comment
+          )
+      )
+    );
   }
 
-  *changeForecastSum(category: Category, month: number, year: number, sum: number): Generator<Promise<Response>> {
+  async changeForecastSum(
+    category: Category,
+    month: number,
+    year: number,
+    sum: number
+  ): Promise<Response> {
     const forecast = this.forecasts.find(
-      f => f.category.id === category.id && f.month === month && f.year === year
-    )
+      (f) =>
+        f.category.id === category.id && f.month === month && f.year === year
+    );
     if (forecast) {
-      forecast.sum = sum
-      yield fetch(
+      forecast.sum = sum;
+      return fetch(
         `${process.env.REACT_APP_API_URL}/forecast?category_id=${category.id}&month=${month}&year=${year}`,
         {
-          method: "PATCH", body: JSON.stringify({ sum }),
+          method: "PATCH",
+          body: JSON.stringify({ sum }),
           headers: {
-            "content-type": "application/json"
-          }
-        })
+            "content-type": "application/json",
+          },
+        }
+      );
     } else {
-      this.forecasts.push(new Forecast(category, month, year, sum, ''))
-      yield fetch(
-        `${process.env.REACT_APP_API_URL}/forecast`,
-        {
-          method: "POST", body: JSON.stringify({
-            category_id: category.id,
-            month,
-            year,
-            sum
-          }),
-          headers: {
-            "content-type": "application/json"
-          }
-        })
+      this.forecasts.push(new Forecast(category, month, year, sum, ""));
+      return fetch(`${process.env.REACT_APP_API_URL}/forecast`, {
+        method: "POST",
+        body: JSON.stringify({
+          category_id: category.id,
+          month,
+          year,
+          sum,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
     }
   }
 
-  *changeForecastComment(category: Category, month: number, year: number, comment: string): Generator<Promise<Response>> {
+  async changeForecastComment(
+    category: Category,
+    month: number,
+    year: number,
+    comment: string
+  ): Promise<Response> {
     const forecast = this.forecasts.find(
-      f => f.category.id === category.id && f.month === month && f.year === year
-    )
+      (f) =>
+        f.category.id === category.id && f.month === month && f.year === year
+    );
     if (forecast) {
-      forecast.comment = comment
-      yield fetch(
+      forecast.comment = comment;
+      return fetch(
         `${process.env.REACT_APP_API_URL}/forecast?category_id=${category.id}&month=${month}&year=${year}`,
         {
-          method: "PATCH", body: JSON.stringify({ comment }),
+          method: "PATCH",
+          body: JSON.stringify({ comment }),
           headers: {
-            "content-type": "application/json"
-          }
-        })
+            "content-type": "application/json",
+          },
+        }
+      );
     } else {
-      this.forecasts.push(new Forecast(category, month, year, 0, comment))
-      yield fetch(
-        `${process.env.REACT_APP_API_URL}/forecast`,
-        {
-          method: "POST", body: JSON.stringify({
-            category_id: category.id,
-            month,
-            year,
-            sum: 0,
-            comment
-          }),
-          headers: {
-            "content-type": "application/json"
-          }
-        })
+      this.forecasts.push(new Forecast(category, month, year, 0, comment));
+      return fetch(`${process.env.REACT_APP_API_URL}/forecast`, {
+        method: "POST",
+        body: JSON.stringify({
+          category_id: category.id,
+          month,
+          year,
+          sum: 0,
+          comment,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
     }
   }
-  
-  transferPersonalExpense(categoryId: PersonalExpCategoryIds, month: number, year: number) {
-    const category = categories.getById(categoryId)
-    const { month: prevMonth, year: prevYear } = getPreviousMonth(month, year)
-    const prevMonthForecast = this.find(prevYear, prevMonth, category)
+
+  async transferPersonalExpense(
+    categoryId: PersonalExpCategoryIds,
+    month: number,
+    year: number
+  ): Promise<Response | undefined> {
+    const category = categories.getById(categoryId);
+    const { month: prevMonth, year: prevYear } = getPreviousMonth(month, year);
+    const prevMonthForecast = this.find(prevYear, prevMonth, category);
     if (!prevMonthForecast) {
-      alert('Сначала заполните прогноз за прошлый месяц!')
-      return
+      alert("Сначала заполните прогноз за прошлый месяц!");
+      return;
     }
-    const prevMonthSpends = roundCost(expenseStore.expenses
-      .filter(e => e.date.month() === prevMonth && e.date.year() === prevYear && e.category.id === categoryId)
-      .reduce((a, c) => a + (c.cost || 0), 0))
-    
-    const forecast = this.find(year, month, category)
-    const sum = prevMonthForecast.sum - prevMonthSpends
-    if (forecast) {
-      forecast.sum = sum
-    } else {
-      this.forecasts.push(new Forecast(category, month, year, sum))
-    }
+    const prevMonthSpends = roundCost(
+      expenseStore.expenses
+        .filter(
+          (e) =>
+            e.date.month() === prevMonth &&
+            e.date.year() === prevYear &&
+            e.category.id === categoryId
+        )
+        .reduce((a, c) => a + (c.cost || 0), 0)
+    );
+
+    const sum = roundCost(prevMonthForecast.sum - prevMonthSpends);
+    return this.changeForecastSum(category, month, year, sum);
   }
 }
 
-const forecastStore = new ForecastStore()
+const forecastStore = new ForecastStore();
 
-export default forecastStore
+export default forecastStore;
