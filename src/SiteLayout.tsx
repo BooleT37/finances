85ce1fc77f32d81decf04ac8b69db7e579c4cba8
Alexey Levin <BooleT37@gmail.com>
2022-10-04 15:50:32 +0200

    feat(site-layout): FIx menu console error

--- src/SiteLayout.tsx
+++ src/SiteLayout.tsx
@@ -6,21 +6,28 @@ import {
   CalendarOutlined,
   DollarOutlined,
   SettingOutlined,
+  BankOutlined,
 } from "@ant-design/icons";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { ItemType } from "antd/lib/menu/hooks/useItems";
 
 const { Content, Footer, Sider } = Layout;
 
-function getItem(label: React.ReactNode, path: string, icon?: React.ReactNode) {
-  return (
-    <Menu.Item key={path} icon={icon}>
-      <Link to={path}>{label}</Link>
-    </Menu.Item>
-  );
+function getItem(
+  label: string,
+  path: string,
+  icon?: React.ReactNode
+): ItemType {
+  return {
+    key: label,
+    icon,
+    label: <Link to={path}>{label}</Link>,
+  };
 }
 
-const items: React.ReactNode[] = [
+const items: ItemType[] = [
   getItem("Данные", "/screens/data", <TableOutlined />),
+  getItem("Траты из сбережений", "/screens/saving-spendings", <BankOutlined />),
   getItem("Статистика", "/screens/statistics", <LineChartOutlined />),
   getItem("Планирование", "/screens/planning", <CalendarOutlined />),
   getItem("Подписки", "/screens/subscriptions", <DollarOutlined />),
@@ -31,22 +38,22 @@ const items: React.ReactNode[] = [
 const SiteLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [collapsed, setCollapsed] = React.useState(false);
   const location = useLocation();
-  let navigate = useNavigate();
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+      <Sider
+        width={210}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={setCollapsed}
+      >
         <div className="logo" />
         <Menu
           selectedKeys={[location.pathname]}
-          onSelect={({ key }) => {
-            navigate(key);
-          }}
           theme="dark"
           mode="inline"
-        >
-          {items}
-        </Menu>
+          items={items}
+        />
       </Sider>
       <Layout className="site-layout">
         <Content>{children}</Content>
