fb1ea242cb217a87b1be86b43d42afc1d50215b2
Alexey Levin <BooleT37@gmail.com>
2022-10-06 20:05:22 +0200

    Enable error boundaries

--- src/configureMobx.ts
+++ src/configureMobx.ts
@@ -1,11 +1,10 @@
-import { configure } from "mobx"
+import { configure } from "mobx";
 
 export default function configureMobx() {
-    configure({
-        enforceActions: "always",
-        computedRequiresReaction: true,
-        reactionRequiresObservable: true,
-        observableRequiresReaction: true,
-        disableErrorBoundaries: true
-    })
+  configure({
+    enforceActions: "always",
+    computedRequiresReaction: true,
+    reactionRequiresObservable: true,
+    observableRequiresReaction: true,
+  });
 }
