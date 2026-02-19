2a2ffaa2835507434f6bae9abe202974ae8c0851
Alexey Levin <BooleT37@gmail.com>
2026-02-19 11:35:49 +0100

    Small changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,4 @@ import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [tsconfigPaths(), tanstackStart(), nitro(), viteReact()],
-  nitro: {
-    serverDir: "./",
-  },
 });
